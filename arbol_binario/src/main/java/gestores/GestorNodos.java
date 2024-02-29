eef883c31d4f095b7802cedba7e798105cb44812
GasconiXx <agasconcrespo@gmail.com>
2024-02-29 14:01:42 +0100

    He añadido el gestor de nodos

---
+++ arbol_binario/src/main/java/gestores/GestorNodos.java
@@ -0,0 +1,14 @@
+package gestores;
+
+import java.util.HashMap;
+
+import modelos.Nodo;
+
+public class GestorNodos {
+
+    private static HashMap<Nodo, Boolean> nodos = new HashMap<>();
+
+    public static boolean addNodo(Nodo nodo, boolean existe) {
+        
+    }
+}
